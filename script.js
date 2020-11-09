953e1460c30702f16a214c0c83c932238b3aebf1
nikolatrajkovicITS <nikolatrajkovic.contact@gmail.com>
2020-11-10 00:34:19 +0100

    Added loader

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // Get Quote from API
 async function getQuote() {
+  loading();
   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -19,10 +35,13 @@ async function getQuote() {
     } else {
       quoteText.classList.remove("long-quote");
     }
-
     quoteText.innerText = data.quoteText;
+
+    // Stop Loader, Show Quote
+    complete();
   } catch (error) {
     getQuote();
+
     console.error("Whoops, no quote", error);
   }
 }
@@ -38,3 +57,6 @@ function tweetQuote() {
 // Event Listeners
 newQuoteBtn.addEventListener("click", getQuote);
 twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuote();
